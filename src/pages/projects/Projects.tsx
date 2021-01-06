a68b1698b2c86136aa2be0723c36570189b327ce
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-01-06 12:00:54 -0300

    Tela de projetos funcionando

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import { Repositories } from './Styles';
+
+interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  owner: {
+    avatar_url: string;
+    login: string;
+  };
+  language: string;
+}
 
 function Projects() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    api.get<Repository[]>('users/gfilenga/repos')
+      .then(response => {
+        console.log(response.data);
+        const repositoriesReturned = response.data;
+
+        let repositories2: Repository[] = repositoriesReturned;
+
+        setRepositories(repositories2);
+      });
+  }, [])
+
   return (
-    <div>
-      <h1>Projetos</h1>
-    </div>
+    <Repositories>
+      {repositories.map(repository => (
+        <a
+          href="teste"
+          key={repository.id}
+        >
+          <img
+            src={repository.owner.avatar_url}
+            alt={repository.owner.login}
+          />
+          <div>
+            <strong>{repository.name}</strong>
+            <p>{repository.description}</p>
+          </div>
+        </a>
+      ))}
+    </Repositories>
   )
 }
 
